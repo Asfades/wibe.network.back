66ed1531066ca69a796a98a5d5cae45f71963c71
Serhii Horbenko <serhiihorbenko@Serhiis-MacBook-Pro.local>
2020-08-08 00:35:13 +0300

    Remove unused imports

--- src/audio/audio.controller.ts
+++ src/audio/audio.controller.ts
@@ -1,26 +1,13 @@
 import {
   Controller,
-  Post,
-  UseInterceptors,
-  UploadedFile,
   Param,
-  UseGuards,
   Body,
-  Patch,
-  Delete,
   Get,
   Res,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
 import { Response } from 'express';
 
-import { editFileName, audioFileFilter } from './utils/audio-upload.utils';
-import { AuthGuard } from '@nestjs/passport';
 import { AudioService } from './audio.service';
-import { GetUser } from './utils/get-user.decorator';
-import { User } from 'src/auth/user.schema';
-import { SaveAudioDto } from './dto/save-audio.dto';
 import { GetUserAudioDto, GetUserAudioValidationPipe } from './dto/get-user-audio.dto';
 
 @Controller('audio')
